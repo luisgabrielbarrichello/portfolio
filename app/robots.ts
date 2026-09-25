69e9928f9860685e7e11813b3057cdac4bc29fba
luisgabrielbarrichello <luisgabrielbarrichello@gmail.com>
2026-09-25 10:19:32 -0300

    fix: update domain in robots and sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://portfolio-6ea.pages.dev/sitemap.xml',
+        sitemap: 'https://luisbarrichello.dev.br/sitemap.xml',
     };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export const dynamic = 'force-static';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://portfolio-6ea.pages.dev',
+            url: 'https://https://luisbarrichello.dev.',
         },
     ];
 }
